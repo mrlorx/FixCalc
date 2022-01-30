43dde6f086545ae13fc93a50e01b22e7e61eb07f
chris <mrlorx@cirro.lan>
2022-01-30 18:41:27 +0100

    Added database builder

--- app/src/main/java/com/mrlorx/fixcalc/storage/ScreenDatabase.kt
+++ app/src/main/java/com/mrlorx/fixcalc/storage/ScreenDatabase.kt
@@ -1,10 +1,33 @@
 package com.mrlorx.fixcalc.storage
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.mrlorx.fixcalc.Screen
 
 @Database(entities = [Screen::class], version = 1)
 abstract class ScreenDatabase : RoomDatabase() {
     abstract fun screenDao(): ScreenDao
+
+    companion object {
+        @Volatile private var INSTANCE: ScreenDatabase? = null
+
+        fun getDatabase(context: Context): ScreenDatabase {
+            val tempInstance =
+                INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context,
+                    ScreenDatabase::class.java,
+                    "screen_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
